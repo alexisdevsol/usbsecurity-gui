5ddc3de85ea2f1c9b2395c7da978d441266eb8a5
Alexis Torres Valdes <alexis89.dev@gmail.com>
2021-05-02 23:46:19 -0400

    Arreglado los clasificadores de pypi

--- usbsecurity_gui/__version__.py
+++ usbsecurity_gui/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'usbsecurity-gui'
 __description__ = 'usbsecurity-gui is the program for the graphical interface of USBSecurity'
 __url__ = 'https://pypi.org/project/usbsecurity-gui/'
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 __author__ = 'Alexis Torres Valdes'
 __author_email__ = 'alexis89.dev@gmail.com'
 __credits__ = ['Alexis Torres Valdes']
