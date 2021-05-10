0eab84ba04450b3863a8b90ca9b6268a220202b3
Alexis Torres Valdes <alexis89.dev@gmail.com>
2021-05-10 11:55:11 -0400

    Nueva rama 'develop'

--- usbsecurity_gui/__version__.py
+++ usbsecurity_gui/__version__.py
@@ -21,7 +21,7 @@
 __title__ = 'usbsecurity-gui'
 __description__ = 'usbsecurity-gui is the program for the graphical interface of USBSecurity'
 __url__ = 'https://pypi.org/project/usbsecurity-gui/'
-__version__ = '4.1.0'
+__version__ = '5.0.0'
 __author__ = 'Alexis Torres Valdes'
 __author_email__ = 'alexis89.dev@gmail.com'
 __credits__ = ['Alexis Torres Valdes']
